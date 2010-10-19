11a6c3ea4c1c91d1d4ca7bce3257a2480210220d
Wes Oldenbeuving <narnach@gmail.com>
2010-10-19 02:11:00 +0200

    Toolbot uses global match state to determine which strategy to use.

--- bots/toolbot.rb
+++ bots/toolbot.rb
@@ -10,9 +10,21 @@ class Toolbot < AI
 
   def do_turn
     super
-    strategy = @strategies[@turn % @strategies.size]
-    log "Picking strategy: #{strategy}"
-    self.send(strategy)
+    log "Reinforce"
+    if @my_ships > @enemy_ships && @my_growth > @enemy_growth
+      log "We have more ships and growth, so go for all-out attack"
+      cap_strategy
+      omni_cap_strategy
+    elsif @my_population > @enemy_population
+      log "We have more ships on our planets than they have, so attack them first"
+      reinforce_strategy
+      cap_strategy
+      omni_cap_strategy
+    else
+      log "We have less ships, so focus on targets of opportunity"
+      reinforce_strategy
+      omni_cap_strategy
+    end
   end
 
   module Toolbox
@@ -37,17 +49,21 @@ class Toolbot < AI
       needed
     end
 
+    # Positive is me, negative is enemy
     def net_ships_underway_to(target)
-      @pw.fleets_underway_to(target).inject(0) {|ships, fleet| fleet.owner == 1 ? ships + fleet.num_ships : ships - fleet.num_ships}
+      @pw.fleets_underway_to(target).inject(0) {|ships, fleet| fleet.mine? ? ships + fleet.num_ships : ships - fleet.num_ships}
     end
   end
   include Toolbox
 
   module ShipsAvailable
+    RESERVE_FACTOR = 5
+
     def do_turn
       super
       self.reset_ships_available
       self.reset_fleets_dispatched
+      self.reset_defenders
     end
 
     def reset_ships_available
@@ -57,6 +73,10 @@ class Toolbot < AI
 
     def reset_fleets_dispatched
       @fleets_dispatched = Hash.new { |hash, key| hash[key] = Array.new }
+    end
+
+    def reset_defenders
+      @defenders = Hash.new { |hash, key| hash[key] = 0 }
     end
 
     def can_attack?(source, num_ships)
@@ -72,9 +92,34 @@ class Toolbot < AI
       @fleets_dispatched[target.planet_id] << Fleet.new(source.owner, num_ships, source.planet_id, target.planet_id, distance, distance)
     end
 
+    def assign_defenders(planet, num_ships)
+      if ships_available(planet) >= num_ships
+        @defenders[planet.planet_id] = num_ships
+        @ships_available[planet.planet_id] -= num_ships
+      else
+        log "!!! Tried to assign more defenders than there are ships on planet #{planet.planet_id}. Want #{num_ships}, have #{ships_available(planet)}"
+      end
+    end
+
+    def ships_for_defense_of(planet)
+      @defenders[planet.planet_id] + ships_available_on(planet)
+    end
+
+    def assign_defenders_to_my_planets
+      @pw.my_planets.each do |planet|
+        reserves = planet.growth_rate * RESERVE_FACTOR
+        ships=net_ships_underway_to(planet)
+        if ships < 0
+          invaders = ships.abs
+          reserves += invaders
+        end
+        assign_defenders(planet, [ships.abs, reserves].min)
+      end
+    end
+
     def attack_with(source, target, num_ships)
       if self.can_attack?(source, num_ships)
-        log "Attacking planet #{target.planet_id} with #{num_ships} ships from planet #{source.planet_id}. Distance is #{@pw.travel_time(source, target)}."
+        log "Attacking planet #{target.planet_id} with #{num_ships} ships from planet #{source.planet_id}. Distance is #{@pw.travel_time(source, target)}, defending ships are #{target.num_ships}."
         @ships_available[source.planet_id] -= num_ships
         @pw.issue_order(source.planet_id, target.planet_id, num_ships)
         dispatch_fleet(source, target, num_ships)
@@ -85,40 +130,37 @@ class Toolbot < AI
   end
   include ShipsAvailable
 
-  # Each of our planets tries to capture the closest non-owned planet. Leaves a small reserve in place and takes incoming ships into account.
+  # Attack the closest enemy planet
   module CapStrategy
-    RESERVES = 0.25
-
-    def do_turn
-      super
-      @pw.my_planets.each do |planet|
-        @ships_available[planet.planet_id] -= (planet.num_ships * RESERVES).to_i
-       end
-    end
-
+    MAX_CAP_PLANETS = 3
+    MAX_SUPPORT_PLANETS = 3
+    
     def cap_strategy
-      @pw.my_planets.each do |source|
-        log "Planet #{source.planet_id} can send #{ships_available_on(source)} ships out to capture planets"
-        @pw.closest_planets(source).each do |target|
-          next if target.mine?
+      @pw.enemy_planets.sort_by{|planet| planet.num_ships}[0...MAX_CAP_PLANETS].each do |target|
+        @pw.my_closest_planets(target)[0...MAX_SUPPORT_PLANETS].each do |source|
           distance = @pw.travel_time(source, target)
           ships_needed = ships_needed_to_capture(target, distance)
-          next if ships_needed <= 0
           ships_to_send = [ships_available_on(source), ships_needed].min
+          next if ships_to_send <= 0
           self.attack_with(source, target, ships_to_send)
         end
       end
     end
   end
-  # include CapStrategy
+  include CapStrategy
 
   # Make a list of targets, then find the closest friendly planets and send ships out from it
   module OmniCapStrategy
     PROXIMITY = 5
-    TOP_X = 3
+    TOP_X = 5
+
+    def do_work
+      super
+      self.assign_defenders_to_my_planets
+    end
 
     def omni_cap_strategy
-      all_my_ships = @pw.my_planets.inject(0) {|ships, planet| planet.num_ships + ships}
+      all_my_ships = @pw.my_planets.inject(0) {|ships, planet| ships_available_on(planet) + ships}
       scores = Hash.new { |hash, key| hash[key] = 0 }
       sums_of_distances = Hash.new { |hash, key| hash[key] = 0 }
       # Find all planets weighed on distance to all friendly planets, growth and hostile fleet count
@@ -148,7 +190,7 @@ class Toolbot < AI
       end
 
       sorted_planets[0...TOP_X].each_with_index do |target, index|
-        log "Target ##{index+1}: planet #{target.planet_id} (G:#{target.growth_rate}, S:#{target.num_ships}, D:#{sums_of_distances[target.planet_id]}) score #{scores[target.planet_id]}"
+        log "Target ##{index+1}: planet #{target.planet_id} (O:#{target.owner}, G:#{target.growth_rate}, S:#{target.num_ships}, D:#{sums_of_distances[target.planet_id]}) score #{scores[target.planet_id]}"
         next if @pw.my_planets.inject(0) {|ships, planet| planet.num_ships + ships} <= target.num_ships
         @pw.my_closest_planets(target).each do |planet|
           available = ships_available_on(planet)
@@ -166,10 +208,42 @@ class Toolbot < AI
   end
   include OmniCapStrategy
 
-  module SnipeStrategy
-    def snipe_strategy
-      # TODO: Scan enemy fleets headed towards a non-enemy planet and send out a fleet that will defeat them after they arrive.
+  module ReinforceStrategy
+    def do_work
+      super
+      self.assign_defenders_to_my_planets
+    end
+
+    # Check for threats to our planets and reinforce them.
+    def reinforce_strategy
+      @pw.my_planets.each do |planet|
+        ships=net_ships_underway_to(planet)
+        next unless ships < 0
+        invaders = ships.abs
+        available = ships_for_defense_of(planet)
+        next if available > invaders
+        assistance_required = invaders - available
+        log "Planet #{planet.planet_id} requires #{assistance_required} ships in assistance to fend off #{invaders} invaders. It has only #{available} ships available."
+        @pw.my_closest_planets(planet).each do |helping_planet|
+          break if assistance_required <= 0
+          next if ships_available_on(helping_planet) <= 0
+
+          distance = @pw.travel_time(helping_planet, planet)
+          if distance <= 1
+            # We can reinforce the planet before any battle occurs
+            ships_to_send = [ships_available_on(helping_planet), assistance_required].min
+            self.attack_with(helping_planet, planet, ships_to_send)
+            assistance_required -= ships_to_send
+          else
+            # Assume our planet will be conquered next turn and we have to take it back by force
+            growth_bonus = (planet.growth_rate * distance) + 1
+            ships_to_send = [ships_available_on(helping_planet), assistance_required + growth_bonus].min
+            self.attack_with(helping_planet, planet, ships_to_send)
+            assistance_required -= [(ships_to_send - growth_bonus), 0].max
+          end
+        end
+      end
     end
   end
-  # include SnipeStrategy
+  include ReinforceStrategy
 end
--- lib/ai.rb
+++ lib/ai.rb
@@ -40,11 +40,13 @@ class AI
         @turn_start = Time.now
         @pw = PlanetWars.new(map_data)
         begin
-          log_state if self.logging
+          log_state
           do_turn
         rescue => e
           log "#{e.class.name}: #{e.message}"
-          log e.backtrace.first
+          e.backtrace[0...5].each do |line|
+            log line
+          end
         end
         log "Finishing turn"
         @pw.finish_turn
@@ -64,26 +66,26 @@ class AI
   end
 
   def log_state
-    my_growth       = @pw.my_planets.inject(0) {|sum, planet| sum + planet.growth_rate}
-    neutral_growth  = @pw.neutral_planets.inject(0) {|sum, planet| sum + planet.growth_rate}
-    enemy_growth    = @pw.enemy_planets.inject(0) {|sum, planet| sum + planet.growth_rate}
+    @my_growth       = @pw.my_planets.inject(0) {|sum, planet| sum + planet.growth_rate}
+    @neutral_growth  = @pw.neutral_planets.inject(0) {|sum, planet| sum + planet.growth_rate}
+    @enemy_growth    = @pw.enemy_planets.inject(0) {|sum, planet| sum + planet.growth_rate}
 
-    my_population      = @pw.my_planets.inject(0) {|sum, planet| sum + planet.num_ships}
-    neutral_population = @pw.neutral_planets.inject(0) {|sum, planet| sum + planet.num_ships}
-    enemy_population   = @pw.enemy_planets.inject(0) {|sum, planet| sum + planet.num_ships}
+    @my_population      = @pw.my_planets.inject(0) {|sum, planet| sum + planet.num_ships}
+    @neutral_population = @pw.neutral_planets.inject(0) {|sum, planet| sum + planet.num_ships}
+    @enemy_population   = @pw.enemy_planets.inject(0) {|sum, planet| sum + planet.num_ships}
 
-    my_fleets     = @pw.my_fleets.inject(0) {|sum, fleet| sum + fleet.num_ships}
-    enemy_fleets  = @pw.enemy_fleets.inject(0) {|sum, fleet| sum + fleet.num_ships}
+    @my_fleets     = @pw.my_fleets.inject(0) {|sum, fleet| sum + fleet.num_ships}
+    @enemy_fleets  = @pw.enemy_fleets.inject(0) {|sum, fleet| sum + fleet.num_ships}
 
-    my_ships     = my_population + my_fleets
-    enemy_ships  = enemy_population + enemy_fleets
+    @my_ships     = @my_population + @my_fleets
+    @enemy_ships  = @enemy_population + @enemy_fleets
 
-    my_fleet_pct = my_ships == 0 ? 0.0 : 100.0 * my_fleets / my_ships
-    enemy_fleet_pct = enemy_ships == 0 ? 0.0 : 100.0 * enemy_fleets / enemy_ships
+    @my_fleet_pct = @my_ships == 0 ? 0.0 : 100.0 * @my_fleets / @my_ships
+    @enemy_fleet_pct = @enemy_ships == 0 ? 0.0 : 100.0 * @enemy_fleets / @enemy_ships
 
-    log "===== #{self.class.name} v#{self.class.version}"
-    log "Me:      T:%4i P:%4i F:%4i (%4.1f%%) G:%3i" % [my_ships, my_population, my_fleets, my_fleet_pct, my_growth]
-    log "Them:    T:%4i P:%4i F:%4i (%4.1f%%) G:%3i" % [enemy_ships, enemy_population, enemy_fleets, enemy_fleet_pct, enemy_growth]
-    log "Neutral: P:%4i G:%3i" % [neutral_population, neutral_growth]
+    log "===== #{self.class.name} v#{self.class.version}, turn #{@turn}"
+    log "Me:      T:%4i P:%4i F:%4i (%4.1f%%) G:%3i" % [@my_ships, @my_population, @my_fleets, @my_fleet_pct, @my_growth]
+    log "Them:    T:%4i P:%4i F:%4i (%4.1f%%) G:%3i" % [@enemy_ships, @enemy_population, @enemy_fleets, @enemy_fleet_pct, @enemy_growth]
+    log "Neutral: P:%4i G:%3i" % [@neutral_population, @neutral_growth]
   end
 end
