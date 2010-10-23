fa59ea1026e4b8abc9e63384f6b706c66d4f90c1
Wes Oldenbeuving <narnach@gmail.com>
2010-10-23 14:17:27 +0200

    Added ReinforceStrategy v2 to Sniperbot

--- bots/sniperbot.rb
+++ bots/sniperbot.rb
@@ -5,10 +5,12 @@ class Sniperbot < AI
   # v1: Wait and shoot. Win by exhausting the enemy.
   # v2: Go on the offensive when I have more ships than my enemy has
   # v3: Optimize targets of opportunity finding
-  version 3
+  # v4: Added ReinforceStrategy v2
+  version 4
 
   def do_turn
     super
+    reinforce_strategy
     sniper_strategy
     opportunity_strategy
   end
@@ -108,10 +110,64 @@ class Sniperbot < AI
         ships_needed = predictions.last.num_ships + 1
 
         # Send a maximum of 25% of the standing fleet to capture targets of opportunity
-        next unless ships_available_on(source) >= (ships_needed * 4)
+        next unless ships_available_on(source) >= ships_needed
         attack_with(source, target, ships_needed)
       end
     end
   end
   include OpportunityStrategy
+
+  # Taken from Toolbot: improve it and share.
+  module ReinforceStrategy
+    # Check for threats to our planets and reinforce them.
+    # Predict future for all my planets, time to look ahead: max of closest enemy planet, inbound fleets and 1 (for no enemies)
+    # * If future owner is not me, set defenders to all on the planet and look for help
+    # * TODO: Closest planet: Assume they attack with their whole garisson. After the attack, how many ships would I have left? That is the amount of available ships.
+    def reinforce_strategy
+      in_need_of_help = Array.new
+      @pw.my_planets.each do |planet|
+        distance = 1
+        possibly_required_defenders = []
+
+        if furthest_inbound_fleet = @pw.fleets_underway_to(planet).sort{|fleet| fleet.turns_remaining}.last
+          distance = furthest_inbound_fleet.turns_remaining if furthest_inbound_fleet.turns_remaining > distance
+        end
+        if closest_enemy = @pw.closest_enemy_planets(planet).first
+          distance_to_closest_enemy = @pw.travel_time(planet, closest_enemy)
+          distance = distance_to_closest_enemy if distance_to_closest_enemy > distance
+        end
+
+        predictions = predict_future_population(planet, distance)
+
+        if predictions.all? {|future| future.mine?}
+          available_ships = predictions.map{|future| future.num_ships}.min
+          possibly_required_defenders << (ships_for_defense_of(planet) - available_ships)
+        elsif predictions.last.mine?
+          # Ownerships will flip back and forth, so keep all ships as defense force
+          possibly_required_defenders << ships_for_defense_of(planet)
+        else
+          possibly_required_defenders << ships_for_defense_of(planet)
+          in_need_of_help << planet
+        end
+
+        if closest_enemy
+          available_ships = predictions[distance_to_closest_enemy].num_ships - closest_enemy.num_ships
+          available_ships = 0 if available_ships < 0
+          possibly_required_defenders << (ships_for_defense_of(planet) - [available_ships, ships_available_on(planet)].min)
+        end
+
+        defenders = possibly_required_defenders.max
+        next if defenders <= 0
+        log "Planet #{planet.planet_id} assigns #{defenders} our of #{ships_available_on(planet)} ships to defense"
+        assign_defenders(planet, defenders)
+      end
+
+      in_need_of_help.each do |target|
+        log "Planet #{target.planet_id} is in need of help"
+        # Explicit code to send aid seems not yet required, as Sniperbot keeps winning against all my other bots.
+        # Sniping and opportunity checking code also fills in to aid where it is economical.
+      end
+    end
+  end
+  include ReinforceStrategy
 end
