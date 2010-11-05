9a44017eb107b5cfdcf093233ecf9ea324b3b76c
Wes Oldenbeuving <narnach@gmail.com>
2010-11-05 12:19:58 +0100

    Laid groundwork for Thinkerbot's attack planning

--- bots/thinkerbot.rb
+++ bots/thinkerbot.rb
@@ -18,28 +18,84 @@ class Thinkerbot < AI
   # It instead uses a query-response between planets and the planner to determine if there is enough support for each sub-strategy it has.
   # This strategy will consider multiple strategies and pick the one it deems best.
   module MasterplanStrategy
+    class AttackTarget
+      attr_accessor :target
+
+      def initialize(target)
+        @target=target
+      end
+
+      def future
+        @future ||= predict_future_population(target, max_distance)
+      end
+
+      def plans
+        @plans ||= Array.new
+      end
+
+      def max_distance
+        @max_distance ||= (
+          d = @pw.travel_time(closest_inhabited_planets.last, target)
+          if inbound_fleets.any?
+            d = [d,closest_inbound_fleets.last.turns_remaining].max
+          end
+          d
+        )
+      end
+
+      def closest_inhabited_planets
+        @closest_inhabited_planets ||=@pw.closest_planets(target, @pw.my_planets + @pw.enemy_planets)
+      end
+
+      def closest_inbound_fleets
+        @closest_inbound_fleets ||= @pw.fleets_underway_to(target).sort_by{|fleet| fleet.turns_remaining}
+      end
+    end
+
+    class AttackPlan
+      attr_accessor :source, :attack_target
+      def initialize(source, attack_target)
+        @source, @attack_target = source, attack_target
+      end
+    end
+
     def do_turn
       super
+      @attack_targets = []
+      @attack_plans = Hash.new { |hash, key| hash[key] = Array.new }
     end
 
     def masterplan_strategy
-      # 1. Set goals to capture all enemy planets
-      # 2. Set goals to capture obvious targets
-      # 3. Set goals to capture convenient targets
-      # 4. Ask all planets how they can support each goal
-      # 5. Determine which combination of planet-support to all possible goals will yield the most desirable outcome
-      #    1. Future predictions of current planets, fleets and proposed fleets: which combination of goals yields to the most desirable output?
-      #    2. Future prediction as in 1, but account for the enemy sniping my targets
-      #    3. Future prediction as in 2, but account for the enemy also going after nearby targets
-      #
-      # * Assign roles to planets
-      #   * Safe supply planet: no targets nearby, so send all ships not required for immediate defense
-      #   * Border planet: no interesting targets are nearby, but it is near enough to enemies that it requires an active defense force.
-      #   * Staging planet: Supply planets should send their ships here.
-      #   * Target of opportunity: non-owned planet that is not expected to receive significant reinforcements. After capture it is expected to become a safe supply planet.
-      #   * Enemy stronghold: expect heavy resistance. Expect all nearby planets to lend support to this planet when attacking.
-      #   * Behind enemy lines: you don't have a chance of capturing these planets, as you can never reinforce them.
-      # * Future-predict forked-attack scenarios. When you attack two far away targets and the enemy can only defend one, you should capture at least one.
+      determine_attack_targets
+      ask_for_sources
+      exclude_impossible_targets
+      match_sources_to_targets
+      attack_possible_targets
+    end
+
+    protected
+
+    def determine_attack_targets
+      @attack_targets = @pw.not_my_planets.map do |planet|
+        AttackTarget.new(planet)
+      end
+    end
+
+    def ask_for_sources
+      @pw.my_planets.each do |planet|
+        @attack_targets.each do |attack_target|
+          attack_target.plans << AttackPlan.new(source, attack_target)
+        end
+      end
+    end
+
+    def exclude_impossible_targets
+    end
+
+    def match_sources_to_targets
+    end
+
+    def attack_possible_targets
     end
   end
   include MasterplanStrategy
