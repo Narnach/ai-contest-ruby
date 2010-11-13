64e1295ad50007d035e87330e85b08655ed1000d
Wes Oldenbeuving <narnach@gmail.com>
2010-11-13 17:42:31 +0100

    Grower v4: added supply lines

--- bots/grower.rb
+++ bots/grower.rb
@@ -4,19 +4,22 @@ class Grower < AI
   attr_reader :target
   bot 'grower'
   # v1: Naieve, agressive
-  # v2: Calculating, agressive
+  # v2: From scratch: Calculating, agressive
   # v3: v2 + simple defense strategy
-  version 3
+  # v4: v3 + supply lines
+  version 4
 
   def do_turn
     super
     @needs_defense = []
 
     defend
+    supply
     attack
   end
 
   def defend
+    log "Assigning defenders"
     @pw.my_planets.each do |planet|
       futures = predict_future_population(planet)
       if futures.all?{|future| future.mine?}
@@ -28,8 +31,27 @@ class Grower < AI
       end
     end
   end
+  
+  def supply
+    log "Supplying planets"
+    # for every friendly
+    @pw.my_planets.each do |source|
+      next if ships_available_on(source) <= 0
+      # find the closest enemy planet
+      next unless enemy = @pw.closest_enemy_planets(source).first
+      distance = @pw.travel_time(source, enemy)
+      # find the closest friendly that is closer to that enemy than me
+      next unless friendly = @pw.my_closest_planets(source).find{|planet| @pw.travel_time(planet, enemy) < distance}
+      distance_to_friendly = @pw.travel_time(source, friendly)
+      # if there are no non-owned planets closer than the friendly planet
+      next if @pw.nearby_planets(source, distance_to_friendly, @pw.not_my_planets).any?
+      # send all available ships to the friendly planet
+      attack_with(source, friendly, ships_available_on(source))
+    end
+  end
 
   def attack
+    log "Attacking the enemy"
     easiest_planets_to_capture(@needs_defense + @pw.not_my_planets, :pruning=>false).each do |target|
       @pw.my_closest_planets(target).each do |source|
         distance = @pw.travel_time(target, source)
