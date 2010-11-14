981436cd2f46c199e909b355ed408022d064bf62
Wes Oldenbeuving <narnach@gmail.com>
2010-11-14 13:20:46 +0100

    Bullybot v2: 1 fleet per planet, targets the closest and weakest planet

--- bots/bullybot.rb
+++ bots/bullybot.rb
@@ -2,16 +2,22 @@ class Bullybot < AI
   include Toolbox
   include ShipsAvailable
   bot 'bullybot'
+  # v1: send 1 fleet at a time
+  # v2: send 1 fleet per planet to the closest and weakest planet
+  version 2
 
   def do_turn
     super
     # If we have a fleet in flight, do nothing
-    return if @pw.my_fleets.any?
-    # Pick my strongest planet
-    source = @pw.my_planets.sort_by{|planet| -planet.num_ships}.first
-    # Pick weakest other planet
-    target = @pw.not_my_planets.sort_by{|planet| planet.num_ships}.first
-    # Send half the source planet's ships away
-    attack_with(source, target, source.num_ships / 2)
+    @pw.my_planets.each do |source|
+      # Skip planets with an outbound fleet
+      next if @pw.my_fleets.find{|fleet| fleet.source_planet == source.planet_id}
+      # Pick weakest other planet
+      target = @pw.not_my_planets.sort_by{|planet| planet.num_ships}.first
+      # Attempt to find an equally close planet nearby
+      target = @pw.closest_planets(source, @pw.not_my_planets).find{|planet| planet.num_ships == target.num_ships}
+      # Send half the source planet's ships away
+      attack_with(source, target, source.num_ships / 2)
+    end
   end
 end
